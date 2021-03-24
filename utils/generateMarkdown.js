cbdfaff39aa3ae4b92ced30d29d94fadd79621b0
Michael O'Connor <oconnor97@aol.com>
2021-03-24 12:18:21 -0400

    editing the renderLicenseBadge and licnese link functions

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  return `![badge](https://img.shields.io/badge/license-${data.license}-brightgreen)`
+  
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+  if(data.license === 'MIT') {
+    return '(https://opensource.org/licenses/MIT)'
+  } else if(data.license === 'Apache 3.0') {
+    return '(https://opensource.org/licenses/Apache-2.0)'
+  } else if(data.license === 'GPL 3.0') {
+    return '(https://www.gnu.org/licenses/gpl-3.0)'
+  } else if(data.license === 'BSD 3') {
+    return '(https://opensource.org/licenses/BSD-3-Clause)'
+  } else {return ''}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,7 +26,16 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.projectName}
-
+  ## Table of Contents
+   * Description
+   * Technologies
+   * Licenses
+   * Installation
+   * Usage
+   * Constibutions
+   * Support
+  
+  
   ## Description 
    * ${data.description}
   
@@ -27,7 +49,9 @@ function generateMarkdown(data) {
   * FS
   
   ## Licenses 
+  
    * ${data.license}
+   * ${renderLicenseBadge(data)}
   
   ## Instalation 
   
